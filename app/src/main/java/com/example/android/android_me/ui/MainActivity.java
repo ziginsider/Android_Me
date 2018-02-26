2fc659209fa736612fe27cff8718b8983513308e
ziginsider <ziginsider@gmail.com>
2018-02-26 19:11:53 +0300

    add MasterListFragment

---
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.android_me.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.android_me.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
